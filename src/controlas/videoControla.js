9d5e8183435d447424a47c3d1a0f07c9780980b6
jbinyim12@naver.com <jbinyim12@naver.com>
2024-06-07 15:12:18 +0900

    240607

--- src/controlas/videoControla.js
+++ src/controlas/videoControla.js
@@ -1,14 +1,53 @@
+const videos = [
+  {
+    id: 1,
+    title: "First Video",
+    createdAt: "minutes ago",
+    views: 59,
+    comment: 2,
+    rating: 5,
+  },
+  {
+    id: 2,
+    title: "Second Video",
+    createdAt: "minutes ago",
+    views: 1,
+    comment: 2,
+    rating: 5,
+  },
+  {
+    id: 3,
+    title: "Third Video",
+    createdAt: "minutes ago",
+    views: 59,
+    comment: 2,
+    rating: 5,
+  },
+];
 export const trending = (req, res) => {
-  return res.render("home");
+  return res.render("home", {
+    pageTitle: "Home",
+    videos,
+  });
 };
 
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `watching ${video.title}`, video });
 };
 
-export const edit = (req, res) => {
-  return res.send("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/video/${id}`);
 };
 
 export const search = (req, res) => {
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { see, edit, deleteVideo, upload } from "../controlas/videoControla";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  deleteVideo,
+  upload,
+} from "../controlas/videoControla";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
